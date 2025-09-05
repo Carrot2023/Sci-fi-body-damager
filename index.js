9b975892872a0143b430f20312d0e8287f58eb3b
Carrot2023 <147546627+Carrot2023@users.noreply.github.com>
2025-09-05 23:08:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-var bodyShield;
-var larmShield;
-var rarmShield;
-var llegShield;
-var rlegShield;
-
-var headHealth;
-var bodyHealth;
-var larmHealth;
-var rarmHealth;
-var llegHealth;
-var rlegHealth;
-
-var head;
-var body;
-var larm;
-var rarm;
-var lleg;
-var rleg;
-
-var $4arms;
-var larm2;
-var rarm2;
-var larm2Shield;
-var rarm2Shield;
-var larm2Health;
-var rarm2Health;
-
 function headShieldValue() {
   var headShield = document.getElementById('headShield');
-document.getElementById('Test').innerText = headShield;
+document.getElementById('Test').innerText = `headShield`;
 }
