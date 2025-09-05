6f8babdcdabd13d5eb5b9f027b88ca9f23d9f3f2
Carrot2023 <147546627+Carrot2023@users.noreply.github.com>
2025-09-05 23:21:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function headShieldValue() {
-  var headShield = document.getElementById('headShield');
-document.getElementById('Test').innerText = `headShield`;
+  let headShield = document.getElementById('headShield');
+document.getElementById('Test').innerHTML = headShield;
 }
